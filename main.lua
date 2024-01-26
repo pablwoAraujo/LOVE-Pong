209ca87b10b500f84b229119a5df2f16954f3d34
Pablwo Araújo <44266103+pablwoAraujo@users.noreply.github.com>
2024-01-26 12:29:31 -0300

    feat: adicionando uma resolução virtual (biblioteca push)

--- main.lua
+++ main.lua
@@ -1,11 +1,19 @@
+-- Biblioteca para criar uma resolução virtual, em vez do tamanho da janela
+-- https://github.com/Ulydev/push
+push = require "push"
+
 -- Definindo as dimensões da janela
 WINDOW_WIDTH = 1280;
 WINDOW_HEIGHT = 720;
 
+-- Definindo as dimensões da janela virtual
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
 -- Função usada para inicializar o jogo
 function love.load()
-    -- Definindo as configurações da janela do jogo
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+    -- Define a resolução virtual, que será renderizada dentro das dimensões da janela real
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
         vsync = true
@@ -23,6 +31,12 @@ end
 
 -- Função para desenhar na tela
 function love.draw()
+    -- Começando a renderizar na resolução virtual
+    push:apply('start')
+
     -- Desenhando um texto na tela
     love.graphics.printf("Hello Pong!", 0, WINDOW_HEIGHT / 2, WINDOW_WIDTH, "center")
+
+    -- Finalizando a renderização na resolução virtual
+    push:apply('end')
 end
